d25cc43b5f16421b551b4acc157a7477ba379059
bsack23 <bsack23@gmail.com>
2025-09-29 13:32:05 -0400

    Added inputs to Bird class

--- Source/DMS332/Private/Pawns/Bird.cpp
+++ Source/DMS332/Private/Pawns/Bird.cpp
@@ -1,10 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Pawns/Bird.h"
-#include "Camera/CameraComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/SkeletalMeshComponent.h"
+// headers for camera and arm
+#include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+// headers for input stuff
+#include "Components/InputComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputActionValue.h"
 
 // Sets default values
 ABird::ABird() {
@@ -25,7 +31,7 @@ ABird::ABird() {
   BirdMesh->SetupAttachment(GetRootComponent());
   // Uncomment this if we want Player0 will auto-possess this Pawn
   // (E means that it's an element of type Enum - an enumerated list)
-  // AutoPossessPlayer = EAutoRecieveInput::Player0;
+  AutoPossessPlayer = EAutoReceiveInput::Player0;
 
   // make a spring arm for the camera and attach it to the Capsule
   SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
@@ -38,12 +44,54 @@ ABird::ABird() {
 }
 
 // Called when the game starts or when spawned
-void ABird::BeginPlay() { Super::BeginPlay(); }
+void ABird::BeginPlay() {
+  Super::BeginPlay();
+  // casting our new Player Controller
+  if (APlayerController *PlayerController =
+          Cast<APlayerController>(GetController())) {
+    // if the cast is successful
+    // setting up the Blueprint Input Mapping Context for our class
+    if (UEnhancedInputLocalPlayerSubsystem *Subsystem =
+            ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+                PlayerController->GetLocalPlayer())) {
+      Subsystem->AddMappingContext(BirdMappingContext, 0);
+    }
+    // hmm? maybe here?
+    bUseControllerRotationPitch = true;
+    bUseControllerRotationYaw = true;
+  }
+}
 
 // Called every frame
 void ABird::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
 
+// set up the Action Values so they can move the Pawn
+void ABird::Move(const FInputActionValue &Value) {
+  const float DirectionValue = Value.Get<float>();
+  if (Controller && (DirectionValue != 0.f)) {
+    FVector Forward = GetActorForwardVector();
+    AddMovementInput(Forward, DirectionValue);
+  }
+}
+
+// set up the Action Values so the Pawn can look around while moving
+void ABird::Look(const FInputActionValue &Value) {
+  const FVector2D LookAxisValue = Value.Get<FVector2D>();
+  if (GetController()) {
+    AddControllerYawInput(LookAxisValue.X);
+    AddControllerPitchInput(LookAxisValue.Y);
+  }
+}
+
 // Called to bind functionality to input
 void ABird::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent) {
   Super::SetupPlayerInputComponent(PlayerInputComponent);
+  // another type of cast
+  if (UEnhancedInputComponent *EnhancedInputComponent =
+          CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
+    EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered,
+                                       this, &ABird::Move);
+    EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered,
+                                       this, &ABird::Look);
+  }
 }
--- Source/DMS332/Public/Pawns/Bird.h
+++ Source/DMS332/Public/Pawns/Bird.h
@@ -3,6 +3,8 @@
 #pragma once
 // turn off annoying vscode reordering of include files
 // clang-format off
+// make sure that EnhancedInput is added to DMS332.Build.cs before this include
+#include "InputActionValue.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "Bird.generated.h"
@@ -13,6 +15,9 @@ class USkeletalMeshComponent;
 // these two for the camera and arm
 class USpringArmComponent;
 class UCameraComponent;
+// forward declarations for input stuff
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class DMS332_API ABird : public APawn {
@@ -31,6 +36,21 @@ public:
 protected:
   // Called when the game starts or when spawned
   virtual void BeginPlay() override;
+  // forward declared 
+  // add Input Category so it will be easier to find in Blueprints
+  // UE5 style Input Mapping Context
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+  UInputMappingContext *BirdMappingContext;
+  // and two Input Actions
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+  UInputAction *MoveAction;
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+  UInputAction *LookAction;
+
+  // not forward declared - see InputActionValue include above
+  void Move(const FInputActionValue &Value);
+  void Look(const FInputActionValue &Value);
+
 
 private:
   UPROPERTY(VisibleAnywhere)
